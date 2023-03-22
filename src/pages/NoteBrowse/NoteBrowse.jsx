2cfbad01d390b091b0e607ea290b411e96a488a1
Damien Pellerin <damienpellerin19@gmail.com>
2023-03-22 09:21:51 +0100

    V2

--- src/pages/NoteBrowse/NoteBrowse.jsx
+++ src/pages/NoteBrowse/NoteBrowse.jsx
@@ -1,5 +1,16 @@
 import s from "./style.module.css";
+import { TextCard } from "components/TextCard/TextCard";
 
 export function NoteBrowse(props) {
-  return <>NoteBrowse</>;
+  return (
+    <>
+      <TextCard
+        title="test"
+        subtitle="01/O1/2022"
+        content="bladvdfvdkqvbdjqvbdbvdjkqvbjbvdkjqbcjdskqm"
+        onClickTrash={() => alert("Click trash")}
+        onClick={() => alert("Click note")}
+      />
+    </>
+  );
 }
